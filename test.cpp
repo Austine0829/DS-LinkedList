6dd12c837106c7f3c74ce37ec84886be04cfb68b
Austine0829 <160201408+Austine0829@users.noreply.github.com>
2024-08-03 13:04:53 +0800

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,142 @@
+#include <iostream>
+
+using namespace std;
+
+struct Person{
+    string name;
+    int age;
+
+    Person *next;
+};
+
+class Information{
+    private:
+    Person *head;
+
+    public:
+
+    Information();
+    ~Information();
+
+    Information (const Information &other) = delete;
+    Information operator = (const Information &other) = delete;
+
+    void add(string name, int age);
+    void removeAt(int node);
+    void display();
+    void at(int node);
+};
+
+Information :: Information()
+:head{0}
+{
+
+}
+
+Information :: ~Information()
+{
+    if (head != nullptr)
+    {
+        delete head; 
+        head = nullptr;
+    }
+}
+
+void Information :: add(string name, int age){
+
+    Person *add = new Person;
+
+    add->name = name;
+    add->age = age;
+    add->next = nullptr;
+
+    if (head == nullptr)
+    {
+        head = add;
+    }
+
+    else
+    {
+        Person *current = head;
+
+        while (current->next != nullptr)
+        {
+            current = current->next;
+        }
+
+        current->next = add;
+        
+    }
+}
+
+void Information :: removeAt(int node){
+
+    Person *current = head;
+    Person *prev = head;
+    int index = 0;
+
+    while (current != nullptr && index < node)
+    {
+        prev = current;
+        current = current->next;
+        
+        index++;
+    }
+    
+    if (node != 0)
+    {
+        prev->next = current->next;
+        current->next = nullptr;    
+    }
+
+    else
+    {
+        head = current->next;     
+    }
+}
+
+void Information :: display(){
+
+    Person *current = head;
+
+    while (current != nullptr)
+    {
+        cout << "Name: " << current->name << "\n";
+        cout << "Age: " << current->age << "\n";
+        cout << "-----------------------------\n";
+
+        current = current->next;
+    }
+}
+
+void Information :: at(int node){
+
+    Person *current = head;
+    int index = 0;
+
+    while (current != nullptr && index < node)
+    {
+        current = current->next;
+        
+        index++;
+    }
+
+    cout << "Name: " << current->name << "\n";
+    cout << "Age: " << current->age << "\n";
+}
+
+int main(){    
+
+    Information use;
+
+    use.add("Austine", 21);
+    use.add("Feeniks", 24);
+    use.add("Andre", 25);
+    use.add("Bodz", 21);
+
+    use.removeAt(0);
+
+    use.display();
+    
+    return 0;
+}
